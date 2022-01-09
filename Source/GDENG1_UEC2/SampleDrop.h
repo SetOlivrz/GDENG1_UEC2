418d2db288b83450ab42e77c768d749c1b9cf4f0
denise_taiza <maria_alyssa_taiza@dlsu.edu.ph>
2022-01-09 19:33:31 +0800

    Added drop spawn + remove debris

---
+++ Source/GDENG1_UEC2/SampleDrop.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DropParentReference.h"
+#include "SampleDrop.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GDENG1_UEC2_API USampleDrop : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USampleDrop();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	FVector initialSize;
+	FVector initialOrigin;
+
+	FVector newSize;
+	FVector newOrigin;
+
+	bool destroyed = false;
+	bool dropped = false;
+
+	UPROPERTY(EditAnywhere) AActor* actorCopy;
+};
